a32bc7ab7a5e512a4f83238be5ca52632d2531d5
Mackie-s <masaki.shimadu@gmail.com>
2022-05-13 09:21:52 +0900

    delete unnecessary brank fragment and Dayjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import { Quizzes } from "./Components/QuizLists.jsx";
+//dayjsを仮想Dom内で使用したかったため導入
+import Dayjs from "react-dayjs";
 
 export const App = () => {
   const [isShow, setIsShow] = useState(false);
@@ -17,11 +19,10 @@ export const App = () => {
     <>
       <button onClick={onClickStart}>クイズを始める</button>
       <button onClick={onClickReset}>やり直す</button>
+      <Dayjs></Dayjs>
       {isShow && (
         //ここにもフラグメントが必要なのはなぜ？
-        <>
           <Quizzes />
-        </>
       )}
     </>
   );
