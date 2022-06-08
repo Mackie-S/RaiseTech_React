7ef81a1af78c721f20f834e0b3bf4c7d73fe50f9
Mackie-s <masaki.shimadu@gmail.com>
2022-06-08 15:54:55 +0900

    delete MyTab component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,13 @@
 import { Date } from "./Date";
-import { MyTabs } from "./MyTabs";
 
 export const Header = () => {
   return (
     <header className="flex justify-center bg-blue-900/20 p-5">
-      <div className="flex justify-between max-w-7xl w-full">
+      <div className="flex justify-between max-w-6xl w-full">
         <div>
           <h1 className="text-5xl font-bold mb-5">4択クイズ！</h1>
           <p className="text-xl">君はいくつ正解できるかな？</p>
         </div>
-        <MyTabs />
         <Date />
       </div>
     </header>
