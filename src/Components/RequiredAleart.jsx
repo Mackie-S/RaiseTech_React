9c850b18d959ac65c5a71abcbebc53f7a510100e
Mackie-s <masaki.shimadu@gmail.com>
2022-06-15 10:01:05 +0900

    pending because I wrote the function but not work well

--- src/Components/RequiredAleart.jsx
+++ src/Components/RequiredAleart.jsx
@@ -1,16 +1,27 @@
 // 必須項目を選択する前に集計ボタンを押した際にalertが出るコンポーネント
 // import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { Buttons } from "./Buttons";
 
-
-export const RequiredAleart = () => {
-  const errorMessage = () => { 
-    toast.error("全てに解答してください");
-  }
+export const RequiredAleart = (props) => {
+  const errorMessage = () => {
+    // もし全ての問いに答えていなかったらアラートを出すという関数
+    if (props.category.checked === false) {
+      toast.error("全てに解答してください");
+    }
+  };
 
   return (
     <>
-      <button onClick={errorMessage}>集計する</button>
+      <Buttons
+        onClick={() => {
+          props.onClickShow();
+          errorMessage();
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        正答を表示する
+      </Buttons>
       <Toaster />
     </>
   );
