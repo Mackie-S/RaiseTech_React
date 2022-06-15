576a1efa3657ae7daff9d7053047dcb34a7375a4
Mackie-s <masaki.shimadu@gmail.com>
2022-06-15 10:01:34 +0900

    implemented the main function that I wanted

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import { Header } from "./Components/Header";
 
 export const App = () => {
   const [isShowQuizzes, setIsShowQuizzes] = useState(false);
+  // const [isShowCorrectAnswer, setIsShowCorrectAnswer] = useState(false);
+
   const onClickStart = () => {
-    setIsShowQuizzes(!isShowQuizzes);
-  };
+      setIsShowQuizzes(isShowQuizzes === false ? !isShowQuizzes : isShowQuizzes)
+    };
 
   const onClickReset = () => {
     window.location.reload();
@@ -17,17 +19,8 @@ export const App = () => {
   // 本当はクイズを始めるボタンを押したらそのボタンを消したい
   return (
     <>
-      <Header />
-      <div className="flex justify-center mt-16">
-        <button onClick={onClickStart} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-          クイズを始める
-        </button>
-        <button onClick={onClickReset} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
-          やり直す
-        </button>
-      </div>
+      <Header onClickStart={onClickStart} onClickReset={onClickReset} />
       <div className="flex justify-center">{isShowQuizzes && <MyTabs />}</div>
-
       {/* <RequiredAleart /> */}
     </>
   );
